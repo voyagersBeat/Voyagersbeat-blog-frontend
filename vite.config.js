e0442a85905d8c038646e58b6fcc47576b91b9f7
priyanshu-inu <priyanshuinuu@gmail.com>
2024-12-19 19:14:12 +0530

    newest-1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // Ensure the server binds to all network interfaces
+    port: process.env.PORT || 3000, // Use the port provided by Render or fall back to 3000
+  },
+});
